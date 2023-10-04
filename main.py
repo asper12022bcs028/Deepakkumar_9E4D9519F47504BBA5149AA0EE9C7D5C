533aa9b725059ba1f04a0841b69958fde505f467
asper12022bcs028 <144785548+asper12022bcs028@users.noreply.github.com>
2023-10-04 14:00:30 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,36 +1,21 @@
-#Implement a class called BankAccount that represents a bank account. The class should have private attributes for account number, account holder name, and account balance. Include methods to deposit money, withdraw money, and display the account balance. Ensure that the account balance cannot be accessed directly from outside the class. Write a program to create an instance of the BankAccount class and test the deposit and
-class BankAccount:
-    def __init__(self, account_number, account_holder_name, initial_balance=0.0):
-        self.__account_number = account_number
-        self.__account_holder_name = account_holder_name
-        self.__account_balance = initial_balance
+#Write a function called linear_search_product that takes the list of products and a target product name as input. The function should perform a linear search to find the target product in the list and return a list of indices of all occurrences of the product if found, or an empty list if the product is not found
+def linear_search_product(products, target_product):
+    indices = []
+    for index, product in enumerate(products):
+        if product == target_product:
+            indices.append(index)
+    return indices
 
-    def deposit(self, amount):
-        if amount > 0:
-            self.__account_balance += amount
-            print("Depositedâ‚¹{}. New balance: {}".format(amount,self.__account_balance))
-        else:
-            print("Invalid deposit amount please deposit a positive amount.")
 
-    def withdraw(self, amount):
-        if 0 < amount <= self.__account_balance:
-            self.__account_balance -= amount
-            print("Withdraw â‚¹{}. New balance: {}".format(amount,self.__account_balance))
-        else:
-            print("Invalid withdrawal amount pr insufficient balance.")
 
-    def display_balance(self):
+# Sample list of products
+product_list = ["apple", "banana", "orange", "apple", "grape", "apple"]
 
-      print("Account balance for{}(Account #{}:â‚¹{}.".format(self.__account_holder_name,self.__account_number,self.__account_balance))
-        
+# Target product to search for
+target_product = "apple"
 
-# create an instance of the Bank Account class 
+# Call the function
+result = linear_search_product(product_list, target_product)
 
-account=BankAccount(account_number="1234567890",account_holder_name="jhon",initial_balance=5000.0)
-
-#Test deposit and withdrawal Functionally.
-
-account.display_balance()
-account.deposit(500.0)
-account.withdraw(200.0)
-account.display_balance()
+# Print the result
+print(result)
